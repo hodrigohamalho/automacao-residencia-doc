611735584d1f8676903c25a9d7de641f856a29f1
Karina Macedo <kmacedovarela@gmail.com>
2011-06-15 12:13:57 -0300

    adicionando mapas de navegaçao

---
+++ Mapas de Navegacao/controlar_residencia.brain.mm
@@ -0,0 +1,9 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1308144575782" ID="ID_973060381" MODIFIED="1308144632448" TEXT="Controle de Resid&#xea;ncias">
+<node CREATED="1308144595660" ID="ID_788291832" MODIFIED="1308144630655" POSITION="right" TEXT="Listagem de resid&#xea;ncias, c&#xf4;modos e perif&#xe9;ricos">
+<node CREATED="1308144634670" ID="ID_1835010992" MODIFIED="1308144655975" TEXT="Alterar estado de um perif&#xe9;rico"/>
+<node CREATED="1308144657361" ID="ID_1536884865" MODIFIED="1308144676542" TEXT="Selecionar Resid&#xea;ncia"/>
+</node>
+</node>
+</map>
